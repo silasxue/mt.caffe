50fcd959dbbd73fc1677ed218c97fd9a686eed61
Russell <russell.s.stewart@gmail.com>
2015-08-27 14:49:28 -0700

    Combined 2 download scripts into 1.

--- get_mt.sh
+++ get_mt.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env sh
-# This scripts downloads the ptb data and unzips it.
+# This scripts downloads the mt data and unzips it.
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $DIR
+
+echo "Downloading raw data"
+
+mkdir -p data && cd data
+wget russellsstewart.com/s/mt/run.sh
+wget russellsstewart.com/s/mt/un.en-es.tgz
+tar xf un.en-es.tgz
 
 echo "Downloading processed data"
 
